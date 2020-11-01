eae5bf92f20bb0b829c2ce42660fc4f02c8b6bbf
montaser1998 <montaser268@gmail.com>
2020-11-01 17:01:34 +0200

    Add project files.

---
+++ MunicipalServices/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using MunicipalServices.Data;
+
+[assembly: HostingStartup(typeof(MunicipalServices.Areas.Identity.IdentityHostingStartup))]
+namespace MunicipalServices.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
